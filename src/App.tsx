17e32a8d69cc26c535cc96a472b9787044d99a74
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-04-06 15:06:48 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Banner from './components/Banner'
+import Contact from './components/Contact'
+import Education from './components/Education'
+import Experience from './components/Experience'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skils from './components/Skils'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    <Skils />
+    <Experience />
+    <Projects />
+    <Education />
+    <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Skils.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skils = () => {
+  return (
+    <h1>
+      Skills
+    </h1>
+  )
+}
+
+export default Skils
