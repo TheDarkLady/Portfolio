71fdd00b98b8914f6472831afdff64f5b4adcf47
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-04-07 11:03:12 +0530

    header for desktop is done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import { Container } from "@mui/material";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
-      
-    </div>
-  )
-}
+    <Container
+      sx={{ display: "flex", position: "relative", justifyContent: "center", padding:"20px" }}
+    >
+      <Container
+        sx={{
+          display: {
+            xs:"none",
+            md:"flex"
+          },
+          flexDirection: "row",
+          justifyContent: "space-between",
+          position: "fixed",
+        }}
+      >
+        <Container sx={{fontSize:"30px",
+            fontWeight:"500"}}>Lakshmi Chaithanya</Container>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            fontFamily:"Poppins",
+            gap:"10px"
+          }}
+        >
+          <a>About me</a>
+          <a>Skills & Tools</a>
+          <a>Experience</a>
+          <a>Projects</a>
+          <a>Education</a>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
