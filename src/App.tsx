6536ea8f5d0323c601b7d848eaee11609f901ce7
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-04-10 00:16:50 +0530

    banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import "./App.css";
 import Banner from "./components/Banner";
-import Contact from "./components/Contact";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
+// import Contact from "./components/Contact";
+// import Education from "./components/Education";
+// import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skils from "./components/Skils";
+// import Projects from "./components/Projects";
+// import Skils from "./components/Skils";
 
 function App() {
   return (
     <div style={{ position: "relative", paddingTop: "100px" }}>
       <Navbar />
       <Banner />
-      <Skils />
-      <Experience />
-      <Projects />
-      <Education />
-      <Contact />
+      {/* <Skils /> */}
+      {/* <Experience /> */}
+      {/* <Projects /> */}
+      {/* <Education /> */}
+      {/* <Contact /> */}
     </div>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,34 @@
+import { Button, Container, Typography } from "@mui/material"
 
 const Banner = () => {
   return (
-    <h1>
-        Banner
-    </h1>
+    <Container sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+      <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "start", alignItems: "start" }}>
+        <Typography sx={{fontFamily:"Roboto", fontSize:"50px", fontWeight:"600"}}>Hello, I'm Lakshmi Chaithanya</Typography>
+        <Typography
+          sx={{
+            background: 'linear-gradient(to right, #3E92CC, #945DD6)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            fontWeight: 'bold', 
+            fontSize: '50px',
+            fontFamily: 'Roboto',
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        >
+          Frontend Developer
+        </Typography>
+        <Typography sx={{ fontFamily: "Roboto", fontSize: "20px", fontWeight: "400", width: "500px", textAlign: "left", marginBottom: "50px" }}>
+          I am a frontend developer with a passion for creating beautiful and functional web applications. I have experience in React, JavaScript, and CSS.
+          I am always eager to learn new technologies and improve my skills.
+        </Typography>
+        <Button sx={{background: 'linear-gradient(to right, #3E92CC, #945DD6)', padding:"20px 20px", color:"#fff", borderRadius:"60px"}}>Download CV</Button>
+      </Container>
+      <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "start", alignItems: "center" }}>
+       
+      </Container>
+    </Container>
   )
 }
 
